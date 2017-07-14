d577640d280b9b8c57e08fc53d626cf521a5b131
Nirav <nirav.smartsensesolutions@gmail.com>
2017-07-14 13:28:15 +0000

    .env

---
+++ config/env.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+    'PORT' : 3000,
+
+    'DB_HOST':'',
+    'DB_NAME':'',
+    'DB_PORT':'',
+    'DB_USERNAME':'',
+    'DB_PASSWORD':'',
+
+
+    'BASE_URL':'',
+
+    'algorithm' : '',
+    'expiresIn' : '',
+
+    //JWT Configuration
+    'JWT_SECRET'     : '',
+    'JWT_ALGORITHEM' : '',
+    'TOKEN_EXPIRY'   : '',
+    
+    //JWT Static token
+    STATIC_TOKEN     : '',
+
+    //Pagination
+    SORT_BY          : 'CreatedAt',
+    SORT_ORDER       : 'desc',
+    PAGE_NUMBER      : 1,
+    RECORDS_PER_PAGE : 10
+};
