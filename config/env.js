58ce04c0e91960b25dc31ac8bbe670bfca746c3f
Nirav <nirav.smartsensesolutions@gmail.com>
2017-07-14 13:22:07 +0000

    Update env.js

--- config/env.js
+++ config/env.js
@@ -2,25 +2,25 @@ module.exports = {
 
     'PORT' : 3000,
 
-    'DB_HOST':'mongodb://',
-    'DB_NAME':'techne',
-    'DB_PORT':27017,
-    'DB_USERNAME':'smart',
-    'DB_PASSWORD':'smart123',
+    'DB_HOST':'',
+    'DB_NAME':'',
+    'DB_PORT':'',
+    'DB_USERNAME':'',
+    'DB_PASSWORD':'',
 
 
-    'BASE_URL':'http://localhost:3000/',
+    'BASE_URL':'',
 
-    'algorithm' : 'HS256',
-    'expiresIn' : '2h',
+    'algorithm' : '',
+    'expiresIn' : '',
 
     //JWT Configuration
-    'JWT_SECRET'     : 'Y/pQxq7CPpvGncQiN3avD+hEzczLD6Hkk/Xae2aGRG0=',
-    'JWT_ALGORITHEM' : 'HS256',
-    'TOKEN_EXPIRY'   : '2h',
+    'JWT_SECRET'     : '',
+    'JWT_ALGORITHEM' : '',
+    'TOKEN_EXPIRY'   : '',
     
     //JWT Static token
-    STATIC_TOKEN     : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsImlzcyI6Imh0dHA6Ly9naWZrYXIuY2xvdWRhcHAubmV0OjgzL3YxLjAvbG9naW4iLCJpYXQiOjE0OTc5Mjc5MTUsImV4cCI6MTQ5NzkzMTUxNSwibmJmIjoxNDk3OTI3OTE1LCJqdGkiOiJVYUYxOTF5VHhpT0hlbDd5In0.JzwKPbbeWFM0kVVdKIQ0URDN9Gpd27yROpWBSHQ8c5A',
+    STATIC_TOKEN     : '',
 
     //Pagination
     SORT_BY          : 'CreatedAt',
